edf80fa3b679178445dee6190ebd27e202aeced0
ingramirez820 <ingramirez820@gmail.com>
2021-04-17 02:08:22 -0500

    msgcommit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+jQuery('#camera_wrap').camera({
+
+    // invoked when the transition effect ends
+    onEndTransition: function() {  },  
+  
+    // invoked when the image on a slide has completely loaded
+    onLoaded      : function() {  },  
+      
+    // invoked when the image on a slide start loading
+    onStartLoading    : function() {  },  
+      
+    // invoked when the transition effect starts
+    onStartTransition : function() {  } 
+    
+  });
